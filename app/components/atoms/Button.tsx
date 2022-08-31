e25a063c07aab05bf2a5213b55bb6a1905cbea46
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-08-31 09:33:19 -0400

    fix: ignore highlight class for buttons and use default bg

--- app/components/atoms/Button.tsx
+++ app/components/atoms/Button.tsx
@@ -13,7 +13,7 @@ interface Props
 export default function Button({ children, className, ...props }: Props) {
   return (
     <button
-      className={`btn text-white shadow-md py-1 px-3 rounded-md dark:highlight-white/5 ${className}`}
+      className={`btn bg-slate-400 text-white shadow-md py-1 px-3 rounded-md //dark:highlight-white/5 ${className}`}
       {...props}
     >
       {children}
