370f9b68ee0813041886d79ea7eeb095f7940607
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-12-29 10:14:51 -0500

    feat: add loaders for super admin pages

--- app/routes/super-admin/admins.tsx
+++ app/routes/super-admin/admins.tsx
@@ -1,4 +1,18 @@
+import { json, type LoaderFunction } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import { requireUser, type User } from "~/models/session.server"
+
+type LoaderData = {
+  user: User
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await requireUser(request)
+  return json<LoaderData>({ user })
+}
+
 export const SuperAdminAdmins = () => {
+  const { user } = useLoaderData<LoaderData>() // <typeof loader>
   return <div>SUPER ADMIN ADMINS</div>
 }
 
