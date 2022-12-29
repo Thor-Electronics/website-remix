c5c28c9fe4e43d842a94639017816c0a496f9b4c
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-12-29 09:17:59 -0500

    feat: super admin nav

--- app/components/organisms/SuperAdminNav.tsx
+++ app/components/organisms/SuperAdminNav.tsx
@@ -15,9 +15,12 @@ interface IProps extends HTMLAttributes<HTMLElement> {
 
 export const SuperAdminNav = ({ heroTitle, items, ...props }: IProps) => {
   return (
-    <nav className="SuperAdminNav bg-white rounded-xl p-4 shadow-lg">
+    <nav className="SuperAdminNav bg-white rounded-xl p-3 shadow-lg absolute bottom-2">
       <div className="hero-title hidden">{heroTitle ?? "SUPER ADMIN"}</div>
-      <ul className="items flex items-center justify-center gap-2 overflow-x-auto">
+      <ul className="items flex items-center gap-2 overflow-x-auto">
+        {/* <li className="fake-space-to-fix-css-scroll-bug sm:hidden p-6"></li>
+        <li className="fake-space-to-fix-css-scroll-bug sm:hidden p-6"></li>
+        <solution>NO NEED TO USE THESE FAKE STUFF, I JUST REMOVED JUSTIFY-CNETER SO THAT THE PROBLEM WAS FIXED</solution> */}
         {items.map(i => (
           <Link to={i.to} key={i.to}>
             <li className="item flex flex-col items-center justify-center bg-blue-50 rounded-lg p-2 text-blue-500 shadow-sm">
--- app/routes/super-admin.tsx
+++ app/routes/super-admin.tsx
@@ -4,56 +4,63 @@ import {
   CubeTransparentIcon,
   QrCodeIcon,
   ShieldCheckIcon,
+  StopIcon,
   UsersIcon,
 } from "@heroicons/react/24/solid"
+import { LinksFunction } from "@remix-run/node"
 import { Outlet } from "@remix-run/react"
 import { Copyright } from "~/components/atoms/Copyright"
 import { LogoIcon } from "~/components/atoms/LogoIcon"
 import SuperAdminNav, { NavItems } from "~/components/organisms/SuperAdminNav"
+import superAdminStyles from "~/styles/super-admin.css"
 
-const PREFIX = "/super-admin"
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: superAdminStyles },
+]
+
+const prefix = "/super-admin"
 const iconClassNames = "w-8 h-8"
 const navItems: NavItems[] = [
   {
     icon: <LogoIcon className={iconClassNames} />,
     label: "Dashboard",
-    to: `${PREFIX}/`,
+    to: `${prefix}/`,
   },
   {
     icon: <UsersIcon className={iconClassNames} />,
     label: "Users",
-    to: `${PREFIX}/users`,
+    to: `${prefix}/users`,
   },
   {
     icon: <ShieldCheckIcon className={iconClassNames} />,
     label: "Admins",
-    to: `${PREFIX}/admins`,
+    to: `${prefix}/admins`,
   },
   {
     icon: <CubeTransparentIcon className={iconClassNames} />, // ArrowPathIcon
     label: "OTA Updates",
-    to: `${PREFIX}/ota-updates`,
+    to: `${prefix}/ota-updates`,
   },
   {
     icon: <QrCodeIcon className={iconClassNames} />, // FingerPrintIcon,BanknotesIcon
     label: "Tokens", // subscriptions?
-    to: `${PREFIX}/tokens`,
+    to: `${prefix}/tokens`,
   },
   {
     icon: <BanknotesIcon className={iconClassNames} />,
     label: "Plans",
-    to: `${PREFIX}/plans`,
+    to: `${prefix}/plans`,
   },
   {
     icon: <CreditCardIcon className={iconClassNames} />,
     label: "Invoices",
-    to: `${PREFIX}/invoices`,
+    to: `${prefix}/invoices`,
   },
 ]
 
 export const SuperAdmin = () => {
   return (
-    <div className="SuperAdmin bg-slate-200 h-screen p-2">
+    <div className="SuperAdmin bg-slate-200 h-screen p-2 relative">
       <SuperAdminNav
         heroTitle={
           <>
