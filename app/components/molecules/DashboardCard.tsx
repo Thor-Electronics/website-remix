f41cd594123728f370cfde4a87d2fecacd52c07d
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-12-29 14:59:45 -0500

    feat: dashboard card

---
+++ app/components/molecules/DashboardCard.tsx
@@ -0,0 +1,34 @@
+import { HTMLAttributes, ReactNode } from "react"
+
+interface IProps extends HTMLAttributes<HTMLElement> {
+  icon: ReactNode
+  heroTitle: ReactNode
+}
+
+export const DashboardCard = ({
+  icon,
+  heroTitle,
+  children,
+  className,
+  ...props
+}: IProps) => {
+  return (
+    <div
+      className={`DashboardCard p-2 bg-white shadow-lg rounded-2xl flex-grow ${className}`}
+      {...props}
+    >
+      {/* Could be a separate component */}
+      <div className="head flex gap-1">
+        <div className="icon w-8 h-8">{icon}</div>
+        <h4 className="title text-xl">{heroTitle}</h4>
+      </div>
+      {children}
+      {/* <div className="content">Some Text or Numbers</div>
+      <div className="description">
+        Optional longer description which is so cool!
+      </div> */}
+    </div>
+  )
+}
+
+export default DashboardCard
