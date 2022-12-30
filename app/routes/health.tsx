bd997d1cfe0f4140ebc0b22bb0aac28a78598e80
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-12-30 04:16:35 -0500

    feat: health check route

---
+++ app/routes/health.tsx
@@ -0,0 +1,29 @@
+import type { LoaderArgs } from "@remix-run/node"
+
+import { db } from "~/utils/db.server"
+
+export async function loader({ request }: LoaderArgs) {
+  const host =
+    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host")
+
+  try {
+    const url = new URL("/", `http://${host}`)
+    await Promise.all([
+      db.session.count(),
+      fetch(url.toString(), { method: "HEAD" }).then(r => {
+        if (!r.ok) return Promise.reject(r)
+      }),
+      fetch(
+        `${process.env.NODE_ENV === "production" ? "https" : "http"}://${
+          process.env.CORE_ADDR
+        }/health`
+      ).then(r => {
+        if (!r.ok) return Promise.reject(r)
+      }),
+    ])
+    return new Response("I'm Alive!")
+  } catch (error: unknown) {
+    console.log("healthcheck ❌", { error })
+    return new Response("ERROR", { status: 500 })
+  }
+}
