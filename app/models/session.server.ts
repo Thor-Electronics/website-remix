76ebd8926a95d4db396a4c44194418fb8bbd59d6
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-01-06 11:47:33 -0500

    feat: update user type

--- app/models/session.server.ts
+++ app/models/session.server.ts
@@ -6,7 +6,10 @@ export type User = {
   id: string
   name: string
   email: string
+  phone?: string
   username?: string
+  created_at?: Date
+  updated_at?: Date
 }
 
 const SESSION_SECRET = process.env.SESSION_SECRET
