f62abd85583118696baf1efe4a19ab5271eb55d2
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-01-17 08:53:41 -0500

    feat: use html form for logout so that the page reloads

--- app/components/atoms/LogoutButton.tsx
+++ app/components/atoms/LogoutButton.tsx
@@ -2,9 +2,9 @@ import { Form } from "@remix-run/react"
 import Button, { Props, TextButton } from "./Button"
 
 export const LogoutButton = ({ children = "Logout", ...props }: Props) => (
-  <Form action="/logout" method="post">
+  <form action="/logout" method="post">
     <TextButton type="submit" className="!bg-rose-600" {...props}>
       {children}
     </TextButton>
-  </Form>
+  </form>
 )
