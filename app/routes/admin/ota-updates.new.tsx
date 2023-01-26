811ca9459396a6b26c71477165c1e6209d3dcce8
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-01-25 23:56:51 -0500

    fix: ota file upload

--- app/routes/admin/ota-updates.new.tsx
+++ app/routes/admin/ota-updates.new.tsx
@@ -1,7 +1,10 @@
-import { FolderPlusIcon } from "@heroicons/react/24/solid"
+import {
+  ExclamationTriangleIcon,
+  FolderPlusIcon,
+} from "@heroicons/react/24/solid"
 import { ActionFunction, json, LoaderFunction, redirect } from "@remix-run/node"
 import { useActionData, useLoaderData, useNavigation } from "@remix-run/react"
-import { FormEvent } from "react"
+import { FormEvent, useState } from "react"
 import Button from "~/components/atoms/Button"
 import { getSessionToken } from "~/models/session.server"
 import api from "~/utils/core.server"
@@ -17,34 +20,34 @@ type LoaderData = {
 export const loader: LoaderFunction = async ({ request }) =>
   json<LoaderData>({ sessionToken: await getSessionToken(request) })
 
-export const action: ActionFunction = async ({ request }) => {
-  const form = await request.formData()
-  console.log(
-    "Posting form data to core: ",
-    form,
-    form.entries(),
-    form.values(),
-    form.get("file"),
-    form.get("version")
-  )
-  // return await api
-  //   .adminPostFirmware(new FormData(), await getSessionToken(request))
-  //   .then(res => {
-  //     console.log("RESPONSE: ", res)
-  //     return res
-  //   })
-  //   .then(data => {
-  //     console.log("Response DATA: ", data)
-  //     return redirect("/admin/ota-updates")
-  //   })
-  //   .catch(err => {
-  //     console.error("ERROR Response: ", err, err.response)
-  //     return json<ActionData>(
-  //       { errors: [err.response?.data?.message, err.message] },
-  //       err.response?.status
-  //     )
-  //   })
-}
+// export const action: ActionFunction = async ({ request }) => {
+//   const form = await request.formData()
+//   console.log(
+//     "Posting form data to core: ",
+//     form,
+//     form.entries(),
+//     form.values(),
+//     form.get("file"),
+//     form.get("version")
+//   )
+// return await api
+//   .adminPostFirmware(new FormData(), await getSessionToken(request))
+//   .then(res => {
+//     console.log("RESPONSE: ", res)
+//     return res
+//   })
+//   .then(data => {
+//     console.log("Response DATA: ", data)
+//     return redirect("/admin/ota-updates")
+//   })
+//   .catch(err => {
+//     console.error("ERROR Response: ", err, err.response)
+//     return json<ActionData>(
+//       { errors: [err.response?.data?.message, err.message] },
+//       err.response?.status
+//     )
+//   })
+// }
 
 const chips = [
   { value: "esp8266", displayName: "ESP8266" },
@@ -55,43 +58,54 @@ const chips = [
 ]
 
 export const AdminOTAUpdatesNew = () => {
-  // const { sessionToken } = useLoaderData<LoaderData>()
-  // const navigation = useNavigation()
+  const { sessionToken } = useLoaderData<LoaderData>()
+  const navigation = useNavigation()
   // const actionData = useActionData<ActionData>()
   // console.log("ACTION DATA: ", actionData)
+  const [error, setError] = useState("")
 
-  // const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault()
-  //   console.log("Submitting ...")
-  //   const formData = new FormData(e.currentTarget)
-  //   // we can't use the API since it's a server file :)
-  //   fetch(`${ENV.CORE_URL}/api/v1/admin/firmware-updates`, {
-  //     method: "POST",
-  //     headers: {
-  //       Authorization: `Bearer ${sessionToken}`,
-  //       // ContentType: "multipart/form-data", // https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects#:~:text=Warning%3A%20When,the%20request%20body.
-  //     },
-  //     body: formData,
-  //   })
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       console.log("RES: ", res)
-  //     })
-  //     .catch(err => console.error("ERROR: ", err))
-  // }
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log("Submitting ...")
+    const formData = new FormData(e.currentTarget)
+    // we can't use the API since it's a server file :)
+    fetch(`${ENV.CORE_URL}/api/v1/admin/firmware-updates`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${sessionToken}`,
+        // ContentType: "multipart/form-data", // https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects#:~:text=Warning%3A%20When,the%20request%20body.
+      },
+      body: formData,
+    })
+      .then(async res => {
+        console.log("RES: ", res)
+        const body = await res.json()
+        if (res.status !== 201 || !res.ok)
+          return setError(
+            `Error uploading firmware to core(${res.status} ${res.statusText}): ${body.message}`
+          )
+        window.location.href = "/admin/ota-updates"
+      })
+      .catch(err => {
+        console.error("ERROR: ", err)
+        setError(`Error uploading firmware to core: ${err.toString()}`)
+      })
+  }
 
   return (
     <div className="admin-page">
       <h2 className="page-title">Upoad New Firmware Update(OTA)</h2>
       <form
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
         method="post"
         encType="multipart/form-data"
         className="form max-w-xs mx-auto bg-white rounded-lg shadow-lg px-2 py-3 sm:p-4 flex flex-col items-stretch justify-center gap-4"
       >
-        {/* {actionData && (
-          <p className="error text-rose-600">{actionData.errors.map(e => e)}</p>
-        )} */}
+        {error && (
+          <p className="error bg-rose-100 border border-rose-400 shadow-md shadow-rose-200 rounded-md py-1 px-2 text-rose-600">
+            {error}
+          </p>
+        )}
         <label className="label">
           Chip:
           <select className="input mt-2" name="chip">
@@ -142,9 +156,19 @@ export const AdminOTAUpdatesNew = () => {
           File:
           <input type="file" name="file" required />
         </label>
-        <Button className="bg-primary py-1 px-3 shadow-blue-300 flex items-center justify-center gap-2.5">
-          <FolderPlusIcon className="w-5 h-5" />
-          Upload Firmware
+        <Button
+          className={`${
+            error ? "bg-rose-500 shadow-rose-300" : "bg-primary shadow-blue-300"
+          } py-1 px-3  flex items-center justify-center gap-2.5`}
+          disabled={navigation.state === "submitting"}
+        >
+          {error ? (
+            <ExclamationTriangleIcon className="w-5 h-5" />
+          ) : (
+            <FolderPlusIcon className="w-5 h-5" />
+          )}
+          {error ? "Error Uploading Firmware" : "Upload Firmware"}
+          {error && <ExclamationTriangleIcon className="w-5 h-5" />}
         </Button>
       </form>
     </div>
