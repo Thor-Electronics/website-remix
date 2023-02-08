013a5c623333a4ce3b3d62b804729d89e9f13050
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-02-08 10:45:38 -0500

    feat: dashboard device switch

--- app/components/molecules/SimpleDeviceCard.tsx
+++ app/components/molecules/SimpleDeviceCard.tsx
@@ -1,6 +1,31 @@
-export const SimpleDeviceCard = ({}) => {
+import { HTMLAttributes } from "react"
+import { Device } from "~/types/Device"
+import { Switch } from "../atoms/Switch"
+import { OnlinePulse } from "./DetailedDeviceCard"
+
+interface IProps extends HTMLAttributes<HTMLElement> {
+  data: Device
+  className?: string
+}
+
+export const SimpleDeviceCard = ({ data: d, className, ...props }: IProps) => {
   return (
-    <div className="SimpleDeviceCard">Super Simple Device Card with Switch</div>
+    <div
+      className={`SimpleDeviceCard bg-white border rounded-lg p-2 flex justify-between ${
+        d.isOnline ? "border-green-500" : "border-slate-300"
+      }`}
+    >
+      <h4 className="name font-medium text-start flex items-center gap-2">
+        {d.name}
+        {d.isOnline && <OnlinePulse />}
+      </h4>
+      <div className="switch">
+        <Switch
+          checked={!!d.state.power}
+          onClick={e => (d.state.power = !d.state.power)}
+        />
+      </div>
+    </div>
   )
 }
 // TODO: maybe we'd better create a device card which is only a wrapper and shows some details and then create smaller components for each device type (KEY, KEY2, KEY4, RELAY, RELAY12, etc.) so that the complexity is less. Also custom UI for each typoe of device will be done. For undefined device types or new ones, we can just show a simple UI! That's times better!
--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -1,27 +1,88 @@
+import { HomeModernIcon } from "@heroicons/react/24/solid"
 import { json, LoaderFunction } from "@remix-run/node"
-import { Link, useLoaderData } from "@remix-run/react"
-import Button from "~/components/atoms/Button"
-import { getSessionData } from "~/models/session.server"
+import { useLoaderData } from "@remix-run/react"
+import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket"
+import { SimpleDeviceCard } from "~/components/molecules/SimpleDeviceCard"
+import { getSessionToken } from "~/models/session.server"
 import { Building } from "~/types/Building"
-import { getUserBuildings } from "~/utils/core.server"
+import { Device } from "~/types/Device"
+import { getBuildingDetails, getUserBuildings } from "~/utils/core.server"
 
-// type LoaderData = {
-//   navItems: object[]
-//   // buildings: object
-// }
+const DASHBOARD_BUILDING_ID_KEY = ""
+let WS_URI = "" // "ws://localhost:3993/api/v1/control/echo"
 
-// export const loader: LoaderFunction = async ({ request }) => {
-//   return json<LoaderData>({
-//     buildings: await getUserBuildings((await getSessionData(request)).token),
-//   })
-// }
+type LoaderData = {
+  buildings: Building[]
+  socketToken: string
+  building: Building
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return json<LoaderData>({
+    buildings: await getUserBuildings(await getSessionToken(request)),
+    socketToken: await getSessionToken(request),
+    building: await getBuildingDetails(
+      (
+        await getUserBuildings(await getSessionToken(request))
+      )[0]?.id,
+      await getSessionToken(request)
+    ),
+  })
+}
 
 export const DashboardIndex = () => {
-  // const { buildings } = useLoaderData<LoaderData>()
+  const { buildings, socketToken, building } = useLoaderData<LoaderData>()
+
+  let mostAccessedBuildingId = buildings[0]?.id
+  let mostAccessedBuilding: Building = buildings[0] as Building
+
+  if (typeof window !== "undefined") {
+    mostAccessedBuildingId =
+      localStorage.getItem(DASHBOARD_BUILDING_ID_KEY) ?? buildings[0].id
+  }
+  buildings.map(b => {
+    if (b.id === mostAccessedBuildingId) {
+      mostAccessedBuilding = b as Building
+    }
+  })
+  // console.log("Most Accessed Building is: ", mostAccessedBuilding.name)
+  WS_URI = `${process.env.NODE_ENV === "production" ? "wss" : "ws"}://${
+    ENV.CORE_ADDR
+  }/api/v1/control/manage/${mostAccessedBuilding.id}`
+  // https://www.npmjs.com/package/react-use-websocket
+  const { lastJsonMessage, sendJsonMessage } = useWebSocket(WS_URI, {
+    onOpen: e => {
+      // TODO: send token and authenticate signal "AUTHENTICATE_CLIENT"
+      console.log("WS Connected: ", e)
+    },
+    onClose: e => console.log("WS Closed: ", e),
+    onError: e => console.warn("WS ERROR: ", e),
+    share: true,
+    // filter: () => false,
+    shouldReconnect: e => true,
+    protocols: socketToken, // FIXME: this causes the connection error on chrome, use a better way!
+  })
+
+  // setTimeout(() => sendJsonMessage({ message: "IDK" }), 2000)
+  console.log("New Message: ", lastJsonMessage)
+  // const activities = lastJsonMessage?.data.message || []
+
   return (
     <div className="DashboardIndex text-center">
-      Welcome to the dashboard! You'll see your most recent building here or
-      maybe the most used buildings and shortcuts probably
+      {/* <h1 className="title">Dashboard</h1> */}
+      <div className="building-card">
+        <div className="building-head flex items-center gap-2 mb-4 text-slate-700">
+          <HomeModernIcon className="w-12 h-12" />
+          <h3 className="building-name font-semibold text-xl">
+            {building.name}
+          </h3>
+        </div>
+        <div className="devices-card bg-slate-50 rounded-2xl mx-[-0.5em] p-2 shadow flex flex-col items-stretch gap-2">
+          {building.devices?.map(d => (
+            <SimpleDeviceCard data={d as Device} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
