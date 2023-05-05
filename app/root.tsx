e3160a02579242f12f73d2dc29d89297f77934af
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-05-05 03:45:03 -0400

    feat: added Hotjar

--- app/root.tsx
+++ app/root.tsx
@@ -48,6 +48,21 @@ function Document({
         <Meta />
         <title>{title}</title>
         <Links />
+        {/* Hotjar Tracking Code for https://thor-electronics.ir */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3479118,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </head>
       <body className="min-h-screen bg-slate-100 dark:bg-slate-900 text-slate-800 dark:text-slate-200">
         {/* <NavigatingScreen /> */}
