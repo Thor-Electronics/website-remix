a1057c6fd134c5a19ac2a179ba225753672d7eff
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-05-16 03:59:32 -0400

    feat: try adding sentry

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,9 +4,18 @@ import { Response } from "@remix-run/node"
 import { RemixServer } from "@remix-run/react"
 import { renderToPipeableStream } from "react-dom/server"
 import { getEnv } from "./env.server"
+import * as Sentry from "@sentry/remix"
+import { db } from "~/utils/db.server"
 
 const ABORT_DELAY = 5000
 global.ENV = getEnv()
+
+Sentry.init({
+  dsn: "https://37a5adf4b6d14259bcd4155027edd51a:120d9ea3f6cb44f4b5f8490855ab2d9b@o4505130372890624.ingest.sentry.io/4505130381410304",
+  integrations: [new Sentry.Integrations.Prisma({ client: db })],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+})
 
 export default function handleRequest(
   request: Request,
