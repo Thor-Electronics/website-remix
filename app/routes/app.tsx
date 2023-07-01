ad6d944dd74750f9f36949036ed9dc380014aaa8
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-07-01 08:30:39 -0400

    feat: hide smart transportation icon in app

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -4,63 +4,63 @@ import {
   ArrowRightOnRectangleIcon,
   UserCircleIcon,
   CpuChipIcon,
-} from "@heroicons/react/24/solid"
-import type { LoaderFunction, LinksFunction } from "@remix-run/node"
-import { json } from "@remix-run/node"
+} from "@heroicons/react/24/solid";
+import type { LoaderFunction, LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Link,
   Outlet,
   useLoaderData,
   useRouteError,
   useRouteLoaderData,
-} from "@remix-run/react"
-import { Copyright } from "~/components/atoms/Copyright"
-import { LogoIcon } from "~/components/atoms/LogoIcon"
-import type { FixedNavItem } from "~/components/organisms/FixedNav"
-import { DashboardNav } from "~/components/organisms/DashboardNav"
-import { getSessionToken, requireUser } from "~/models/session.server"
-import dashboardStyles from "~/styles/dashboard.css"
-import type { User } from "~/types/User"
-import type { V2_ErrorBoundaryComponent } from "@remix-run/react/dist/routeModules"
-import { IoCarSport } from "react-icons/io5"
-import api from "~/utils/core.server"
-import type { Device } from "~/types/Device"
+} from "@remix-run/react";
+import { Copyright } from "~/components/atoms/Copyright";
+import { LogoIcon } from "~/components/atoms/LogoIcon";
+import type { FixedNavItem } from "~/components/organisms/FixedNav";
+import { DashboardNav } from "~/components/organisms/DashboardNav";
+import { getSessionToken, requireUser } from "~/models/session.server";
+import dashboardStyles from "~/styles/dashboard.css";
+import type { User } from "~/types/User";
+import type { V2_ErrorBoundaryComponent } from "@remix-run/react/dist/routeModules";
+import { IoCarSport } from "react-icons/io5";
+import api from "~/utils/core.server";
+import type { Device } from "~/types/Device";
 
-export const DASHBOARD_PREFIX = "/app"
+export const DASHBOARD_PREFIX = "/app";
 
 type LoaderData = {
-  user: User
-  orphanDevices: Device[]
-  token: string
-}
+  user: User;
+  orphanDevices: Device[];
+  token: string;
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const token = await getSessionToken(request)
-  const user = await requireUser(request) // todo: optimize it by getting the token once? or getAuth to get both token and user?
+  const token = await getSessionToken(request);
+  const user = await requireUser(request); // todo: optimize it by getting the token once? or getAuth to get both token and user?
   // console.log(`app.tsx -- ${user.name}(${user.id}) is using the app`)
   const orphanDevices = await api
     .getOrphanDevices(token)
-    .then(data => data)
-    .catch(err => {
-      console.error("Error fetching orphan devices: ", err)
-      return []
-    })
-  return json<LoaderData>({ user, orphanDevices, token })
-}
+    .then((data) => data)
+    .catch((err) => {
+      console.error("Error fetching orphan devices: ", err);
+      return [];
+    });
+  return json<LoaderData>({ user, orphanDevices, token });
+};
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: dashboardStyles },
-]
+];
 
 export const Dashboard = () => {
-  const { user, orphanDevices } = useLoaderData<LoaderData>()
+  const { user, orphanDevices } = useLoaderData<LoaderData>();
 
-  const navItems = initialUserNavItems
+  const navItems = initialUserNavItems;
   if (
     orphanDevices.length !== 0 &&
     navItems.indexOf(orphanDeviceNavItem) === -1
   ) {
-    navItems.push(orphanDeviceNavItem)
+    navItems.push(orphanDeviceNavItem);
   }
 
   return (
@@ -69,12 +69,12 @@ export const Dashboard = () => {
       <Outlet />
       <Copyright />
     </div>
-  )
-}
+  );
+};
 
 export const ErrorBoundary: V2_ErrorBoundaryComponent = () => {
-  const error = useRouteError()
-  console.error("Error in dashboard: ", error)
+  const error = useRouteError();
+  console.error("Error in dashboard: ", error);
 
   return (
     <div className="h-screen bg-rose-100 shadow-lg text-rose-600 p-4 flex items-center justify-center flex-col">
@@ -95,13 +95,13 @@ export const ErrorBoundary: V2_ErrorBoundaryComponent = () => {
         Back to the app
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export const useAppLoaderData = () =>
-  useRouteLoaderData("routes/app") as LoaderData
+  useRouteLoaderData("routes/app") as LoaderData;
 
-const iconClassNames = "w-8 h-8"
+const iconClassNames = "w-8 h-8";
 const orphanDeviceNavItem = {
   icon: <CpuChipIcon className={iconClassNames} />,
   label: "Devices",
@@ -109,7 +109,7 @@ const orphanDeviceNavItem = {
   props: {
     className: "!bg-orange-100 !text-orange-400",
   },
-}
+};
 const initialUserNavItems: FixedNavItem[] = [
   {
     icon: <LogoIcon className={iconClassNames} />,
@@ -121,11 +121,11 @@ const initialUserNavItems: FixedNavItem[] = [
     label: "Groups",
     to: `${DASHBOARD_PREFIX}/groups`,
   },
-  {
-    icon: <IoCarSport className={iconClassNames} />,
-    label: "Vehicles",
-    to: `${DASHBOARD_PREFIX}/vehicles`,
-  },
+  // {
+  //   icon: <IoCarSport className={iconClassNames} />,
+  //   label: "Vehicles",
+  //   to: `${DASHBOARD_PREFIX}/vehicles`,
+  // },
   {
     icon: <Cog6ToothIcon className={iconClassNames} />,
     label: "Settings",
@@ -144,6 +144,6 @@ const initialUserNavItems: FixedNavItem[] = [
       className: "!bg-rose-100 !text-rose-500",
     },
   },
-]
+];
 
-export default Dashboard
+export default Dashboard;
