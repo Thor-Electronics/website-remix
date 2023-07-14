f8e6201ff4817953f8890c3e414135938d8502b4
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-07-14 05:10:54 -0400

    feat: show online users, devices, groups

--- app/routes/panel._index.tsx
+++ app/routes/panel._index.tsx
@@ -3,26 +3,26 @@ import {
   CurrencyDollarIcon,
   HomeModernIcon,
   UserGroupIcon,
-} from "@heroicons/react/24/solid"
-import { json, type LoaderFunction } from "@remix-run/node"
-import { useLoaderData } from "@remix-run/react"
-import DashboardCard from "~/components/molecules/DashboardCard"
-import { getSessionToken, requireUser } from "~/models/session.server"
-import { type User } from "~/types/User"
-import api from "~/utils/core.server"
+} from "@heroicons/react/24/solid";
+import { json, type LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import DashboardCard from "~/components/molecules/DashboardCard";
+import { getSessionToken, requireUser } from "~/models/session.server";
+import { type User } from "~/types/User";
+import api from "~/utils/core.server";
 
 type LoaderData = {
-  user: User
-  users: { count: number; online: number; grouped: number }
-  groups: { count: number; online: number }
-  devices: { count: number; online: number; verified: number }
-  totalRevenue: number
+  user: User;
+  users: { count: number; online: number; grouped: number };
+  groups: { count: number; online: number };
+  devices: { count: number; online: number; verified: number };
+  totalRevenue: number;
   latestOTAUpdates: {
-    date: Date
-    size: number // bytes
-    title: string
-    description: string
-  }[] // 3 of them
+    date: Date;
+    size: number; // bytes
+    title: string;
+    description: string;
+  }[]; // 3 of them
   // revenue: {
   //   week: number
   //   month: number
@@ -30,13 +30,13 @@ type LoaderData = {
   //   year: number
   //   total: number
   // }
-}
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const user = await requireUser(request)
+  const user = await requireUser(request);
   const { user: u, dashboard: d } = await api.adminGetInitialData(
     await getSessionToken(request)
-  )
+  );
   return json<LoaderData>({
     user: u,
     users: {
@@ -65,31 +65,31 @@ export const loader: LoaderFunction = async ({ request }) => {
         description: "Start 1402 with new Thor Electronics AI",
       },
     ],
-  })
-}
+  });
+};
 
 export const AdminIndex = () => {
   const { user, users, groups, devices, totalRevenue, latestOTAUpdates } =
-    useLoaderData<LoaderData>() // <typeof loader>
+    useLoaderData<LoaderData>(); // <typeof loader>
 
   const cards = [
     {
       color: "indigo",
       icon: <UserGroupIcon />,
       title: "Users",
-      text: users.count.toLocaleString(),
+      text: `${users.count.toLocaleString()}(${users.online.toLocaleString()})`,
     },
     {
       color: "sky",
       icon: <HomeModernIcon />,
       title: "Groups",
-      text: groups.count.toLocaleString(),
+      text: `${groups.count.toLocaleString()}(${groups.online.toLocaleString()})`,
     },
     {
       color: "pink",
       icon: <CpuChipIcon />,
       title: "Devices",
-      text: devices.count.toLocaleString(),
+      text: `${devices.count.toLocaleString()}(${devices.online.toLocaleString()})`,
     },
     {
       color: "teal",
@@ -97,7 +97,7 @@ export const AdminIndex = () => {
       title: "Revenue",
       text: "$" + totalRevenue.toLocaleString(),
     },
-  ]
+  ];
   return (
     <div className="AdminIndex">
       <h2 className="title font-black font-serif text-3xl text-center mb-4">
@@ -105,7 +105,7 @@ export const AdminIndex = () => {
       </h2>
       <div className="cards flex flex-wrap gap-2">
         <div className="fake-div-to-generate-colors hidden from-indigo-400 to-indigo-600 from-sky-400 to-sky-600 from-pink-400 to-pink-600 from-teal-400 to-teal-600"></div>
-        {cards.map(c => (
+        {cards.map((c) => (
           <DashboardCard
             key={c.title}
             className={`bg-gradient-to-br from-${c.color}-400 to-${c.color}-600 text-white`}
@@ -118,7 +118,7 @@ export const AdminIndex = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminIndex
+export default AdminIndex;
