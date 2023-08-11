22d7ac5d1d8399b7a9d5f3cb49a320c760e825a9
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-08-11 11:22:45 +0330

    chore: update dependencies

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -5,7 +5,7 @@ import { RemixServer } from "@remix-run/react";
 import { renderToPipeableStream } from "react-dom/server";
 import { getEnv } from "./env.server";
 import * as Sentry from "@sentry/remix";
-import { db } from "~/utils/db.server";
+// import { db } from "~/utils/db.server";
 
 const ABORT_DELAY = 5000;
 global.ENV = getEnv();
@@ -13,7 +13,7 @@ global.ENV = getEnv();
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: "https://37a5adf4b6d14259bcd4155027edd51a:120d9ea3f6cb44f4b5f8490855ab2d9b@o4505130372890624.ingest.sentry.io/4505130381410304",
-    integrations: [new Sentry.Integrations.Prisma({ client: db })],
+    // integrations: [new Sentry.Integrations.Prisma({ client: db })],
     // Performance Monitoring
     tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
   });
