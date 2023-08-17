bfbd968dd120f7c81221f4e420aeb379c818aef8
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-08-17 20:41:57 +0330

    fix: simple device card err

--- app/components/molecules/SimpleDeviceCard.tsx
+++ app/components/molecules/SimpleDeviceCard.tsx
@@ -1,27 +1,27 @@
-import { useState, type HTMLAttributes } from "react"
+import { useState, type HTMLAttributes } from "react";
 import type {
   Device,
   DeviceControlPanelStateUpdateHandler,
   DeviceStateUpdateSender,
-} from "~/types/Device"
-import { OnlinePulse } from "../atoms/Pulse"
-import KeyControl from "./KeyControl"
-import TVControl from "./TVControl"
-import { Link } from "@remix-run/react"
-import { DASHBOARD_PREFIX } from "~/routes/app"
+} from "~/types/Device";
+import { OnlinePulse } from "../atoms/Pulse";
+import KeyControl from "./KeyControl";
+import TVControl from "./TVControl";
+import { Link } from "@remix-run/react";
+import { DASHBOARD_PREFIX } from "~/routes/app";
 import {
   ArrowSmallRightIcon,
   EllipsisVerticalIcon,
   PencilIcon,
   TrashIcon,
-} from "@heroicons/react/24/solid"
-import type { DeviceType } from "~/types/DeviceType"
-import SimpleDeviceCardOptionsMenu from "./SimpleDeviceCardOptionsMenu"
-import { IconButton } from "@mui/material"
+} from "@heroicons/react/24/solid";
+import type { DeviceType } from "~/types/DeviceType";
+import SimpleDeviceCardOptionsMenu from "./SimpleDeviceCardOptionsMenu";
+import { IconButton } from "@mui/material";
 
 export interface ISimpleDeviceCardProps extends HTMLAttributes<HTMLElement> {
-  data: Device
-  onUpdate?: DeviceStateUpdateSender
+  data: Device;
+  onUpdate?: DeviceStateUpdateSender;
 }
 
 export const SimpleDeviceCard = ({
@@ -30,23 +30,23 @@ export const SimpleDeviceCard = ({
   onUpdate: updateHandler,
   ...props
 }: ISimpleDeviceCardProps) => {
-  const handleControlUpdate: DeviceControlPanelStateUpdateHandler = cmd => {
-    if (!updateHandler) return false
+  const handleControlUpdate: DeviceControlPanelStateUpdateHandler = (cmd) => {
+    if (!updateHandler) return false;
     // Attach ID to the command before sending it to the server
-    return updateHandler({ ...cmd, id: d.id })
-  }
+    return updateHandler({ ...cmd, id: d.id });
+  };
 
-  const ControlPanel = DeviceControlPanels[d.type]
+  const ControlPanel = DeviceControlPanels[d.type];
 
-  if (d.latency) d.latency = Math.floor(d.latency / 20)
+  if (d.latency) d.latency = Math.floor(d.latency / 20);
 
   /* Options Menu */
   const [optionsMenuAnchorEl, setOptionsMenuAnchorEl] =
-    useState<null | HTMLElement>(null)
-  const optionsMenuOpen = Boolean(optionsMenuAnchorEl)
+    useState<null | HTMLElement>(null);
+  const optionsMenuOpen = Boolean(optionsMenuAnchorEl);
   const handleOptionsClick = (event: React.MouseEvent<HTMLButtonElement>) =>
-    setOptionsMenuAnchorEl(event.currentTarget)
-  const handleCloseOptionsMenu = () => setOptionsMenuAnchorEl(null)
+    setOptionsMenuAnchorEl(event.currentTarget);
+  const handleCloseOptionsMenu = () => setOptionsMenuAnchorEl(null);
 
   return (
     <div
@@ -111,21 +111,32 @@ export const SimpleDeviceCard = ({
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export const DeviceControlPanels: Record<DeviceType, Function> = {
+  GENERAL: () => "GENERAL DEVICE",
+  CORE: () => "CORE PROCESSOR(THOR)",
   KEY: KeyControl,
   KEY1: KeyControl,
   KEY2: KeyControl,
   KEY3: KeyControl,
   KEY4: KeyControl,
-  LOCK: () => "LOCK CONTROL",
-  BELL: () => "BELL CONTROL",
-  TV: TVControl,
+  RELAY: () => "GENERAL RELAY",
+  RELAY8: () => "RELAY CARD(8)",
+  RELAY12: () => "RELAY CARD(12)",
+  PLUG: () => "PLUG / WALL PLUG/ POWER SOCKET",
   LIGHT: () => "LIGHT CONTROL",
-  IRHUB: () => "IR_HUB CONTROL",
-  BLINDS: () => "BLINDS CONTROL",
+  BELL: () => "BELL CONTROL",
+  LOCK: () => "LOCK CONTROL",
   DOOR: () => "DOOR CONTROL",
-  VEHICLE: () => "VEHICLE CONTROL",
-}
+  TV: TVControl,
+  BLINDS: () => "BLINDS CONTROL",
+  CURTAIN: () => "CURTAIN CONTROL",
+  THERMOSTAT: () => "THERMOSTAT CONTROL",
+  HUB_IR: () => "HUB_IR CONTROL",
+  HUB_RF: () => "HUB_RF CONTROL",
+  HUB_LOCAL: () => "HUB_LOCAL CONTROL",
+  DEV: () => "DEVELOPMENT DEVICE",
+  ECU: () => "VEHICLE CONTROL",
+};
