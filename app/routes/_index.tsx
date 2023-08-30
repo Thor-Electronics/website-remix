fb9b4a58d91d7bd93bde08983190a53489608712
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-08-30 15:03:16 -0400

    feat: cleanup

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,12 @@
-import type { LinksFunction, LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import Button, { TextButton } from "~/components/atoms/Button";
-import darkStyles from "~/styles/dark.css";
-import {
-  getOptionalUser,
-  getSessionData,
-  getUserId,
-} from "~/models/session.server";
-import { LogoutButton } from "~/components/atoms/LogoutButton";
-import Hero from "~/components/organisms/Hero";
-import Footer from "~/components/organisms/Footer";
-import About from "~/components/organisms/About";
-import type { User } from "~/types/User";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node"
+import { json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import darkStyles from "~/styles/dark.css"
+import { getOptionalUser, getSessionData } from "~/models/session.server"
+import Hero from "~/components/organisms/Hero"
+import Footer from "~/components/organisms/Footer"
+import About from "~/components/organisms/About"
+import type { User } from "~/types/User"
 
 // ROUTING: https://remix.run/docs/en/1.15.0/file-conventions/route-files-v2#folders-for-organization
 
@@ -23,26 +17,26 @@ export const links: LinksFunction = () => [
     href: darkStyles,
     media: "(prefers-color-scheme: dark)",
   },
-];
+]
 
 type LoaderData = {
-  user?: User;
-  session: Awaited<ReturnType<typeof getSessionData>>;
-};
+  user?: User
+  session: Awaited<ReturnType<typeof getSessionData>>
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const user = await getOptionalUser(request);
-  const session = await getSessionData(request);
+  const user = await getOptionalUser(request)
+  const session = await getSessionData(request)
   // console.log(
   //   `_index.tsx -- ${user?.name ?? "Guest"}(${user?.id ?? "-"}) is visiting ${
   //     request.url
   //   }`
   // )
-  return json<LoaderData>({ user, session });
-};
+  return json<LoaderData>({ user, session })
+}
 
 export default function Index() {
-  const { user } = useLoaderData<LoaderData>();
+  const { user } = useLoaderData<LoaderData>()
 
   return (
     <main className="h-full /max-w-6xl mx-auto text-center snap-mandatory snap-y">
@@ -50,5 +44,5 @@ export default function Index() {
       <About />
       {/* <Footer /> */}
     </main>
-  );
+  )
 }
