adb19a2b0f544662513a755cdd5eeece8fc4241c
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2023-09-02 02:15:23 -0400

    feat: color picker drafts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,6 +7,9 @@ import Hero from "~/components/organisms/Hero"
 import Footer from "~/components/organisms/Footer"
 import About from "~/components/organisms/About"
 import type { User } from "~/types/User"
+import type { RgbColor } from "react-colorful"
+import { HexColorInput, RgbColorPicker } from "react-colorful"
+import { useState } from "react"
 
 // ROUTING: https://remix.run/docs/en/1.15.0/file-conventions/route-files-v2#folders-for-organization
 
@@ -38,9 +41,19 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function Index() {
   const { user } = useLoaderData<LoaderData>()
 
+  // const white: RgbColor = { r: 255, g: 255, b: 255 }
+  // const thorPrimary: RgbColor = { r: 59, g: 130, b: 246 }
+  // const [color, setColor] = useState<RgbColor>(white)
+
   return (
     <main className="h-full /max-w-6xl mx-auto text-center snap-mandatory snap-y">
       <Hero user={(user as User) ?? undefined} />
+      {/* <div>
+        <RgbColorPicker color={color} onChange={setColor} />
+        <span>
+          {color.r}, {color.g}, {color.b}
+        </span>
+      </div> */}
       <About />
       {/* <Footer /> */}
     </main>
