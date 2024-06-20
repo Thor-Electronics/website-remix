a082800f6f2a73b726ff0483726e10c06f5f7735
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2024-06-20 18:51:46 +0330

    fix: env vars

--- app/env.server.ts
+++ app/env.server.ts
@@ -1,12 +1,15 @@
 import invariant from "tiny-invariant";
+import { config } from "dotenv";
+
+config();
 
 export const getEnv = () => {
   const production = process.env.NODE_ENV === "production";
   const secureCore = process.env.SECURE_CORE === "false" ? false : true;
   invariant(process.env.CORE_ADDR, "CORE_ADDR must be defined");
-  // console.log(
-  //   `ENV: Using ${secureCore ? "" : "un"}secure core ${process.env.CORE_ADDR}`
-  // );
+  console.log(
+    `ENV: Using ${secureCore ? "" : "un"}secure core ${process.env.CORE_ADDR}`
+  );
   return {
     CORE_ADDR: process.env.CORE_ADDR,
     CORE_URL: `${secureCore ? "https" : "http"}://${process.env.CORE_ADDR}`,
