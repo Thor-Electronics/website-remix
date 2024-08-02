c8cfac5d8425466058cece91b5bf5c6b05ec058f
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2024-08-02 03:09:16 -0400

    fix: panel error for time ago

--- app/utils/time.ts
+++ app/utils/time.ts
@@ -1,6 +1,6 @@
 export const formatter = new Intl.RelativeTimeFormat(undefined, {
   numeric: "auto",
-})
+});
 
 export const DIVISIONS = [
   { amount: 60, name: "seconds" },
@@ -10,21 +10,21 @@ export const DIVISIONS = [
   { amount: 4.34524, name: "weeks" },
   { amount: 12, name: "months" },
   { amount: Number.POSITIVE_INFINITY, name: "years" },
-]
+];
 
 export const timeAgo = (date: Date): string => {
-  let duration = (date.getTime() - new Date().getTime()) / 1000
+  let duration = (date.getTime() - new Date().getTime()) / 1000;
 
-  for (let i = 0; i <= DIVISIONS.length; i++) {
-    const division = DIVISIONS[i]
+  for (let i = 0; i < DIVISIONS.length; i++) {
+    const division = DIVISIONS[i];
     if (Math.abs(duration) < division.amount) {
       return formatter.format(
         Math.round(duration),
         division.name as Intl.RelativeTimeFormatUnit
-      )
+      );
     }
-    duration /= division.amount
+    duration /= division.amount;
   }
 
-  return date.toString()
-}
+  return date.toString();
+};
